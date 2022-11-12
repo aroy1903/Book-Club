883ac4eac1029761b2ef7e5a84c95f410a36d241
aroy1903 <ananthroy1903@gmail.com>
2022-11-12 13:49:55 -0800

    Fixing Bugs

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -3,12 +3,12 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAEJto5EBExGWg5-CZxmsaQqy8H0AW2800',
-  authDomain: 'bookclub-d3d4a.firebaseapp.com',
-  projectId: 'bookclub-d3d4a',
-  storageBucket: 'bookclub-d3d4a.appspot.com',
-  messagingSenderId: '593970031891',
-  appId: '1:593970031891:web:3f9d206e2a547bccaf3ee6',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
 };
 
 firebase.initializeApp(firebaseConfig);
